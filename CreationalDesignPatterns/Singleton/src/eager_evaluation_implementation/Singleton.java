455956149e13079601b7995209ac1eca58ccf2e3
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-25 17:41:11 +0200

    Singleton Eager Evaluation implementation

---
+++ CreationalDesignPatterns/Singleton/src/eager_evaluation_implementation/Singleton.java
@@ -0,0 +1,11 @@
+package eager_evaluation_implementation;
+
+public class Singleton {
+    private static Singleton uniqueInstance = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return uniqueInstance;
+    }
+}
