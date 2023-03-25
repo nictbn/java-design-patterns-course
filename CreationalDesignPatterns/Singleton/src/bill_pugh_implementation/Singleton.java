a3e71fa6decb50549ba5a15d7d3df53cfd923d77
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-25 18:17:14 +0200

    Singleton Bill Pugh implementation

---
+++ CreationalDesignPatterns/Singleton/src/bill_pugh_implementation/Singleton.java
@@ -0,0 +1,21 @@
+package bill_pugh_implementation;
+
+public class Singleton {
+    private int data = 0;
+    private Singleton() {}
+    private static class SingletonHelper {
+        private static final Singleton uniqueInstance = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHelper.uniqueInstance;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
